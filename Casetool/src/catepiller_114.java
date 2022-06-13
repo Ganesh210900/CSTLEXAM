0b98458ea20c1ddd33509b0040f49d9bab5c75a3
admin <admin@Bcalab3-m02>
2022-06-13 09:21:05 +0530

    commit114

---
+++ Casetool/src/catepiller_114.java
@@ -0,0 +1,16 @@
+//Rbca114
+public class catepiller_114 {
+	int moves,cabbages;
+	
+	catepiller_114()
+	{
+		this.moves=moves;
+		this.cabbages=cabbages;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
